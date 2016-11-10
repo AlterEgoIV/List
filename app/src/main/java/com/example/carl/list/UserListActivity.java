5540a5dd385e1e71af1f72947cfa5a51825bbb5d
RustedBot <alterego01010@gmail.com>
2016-11-10 14:04:00 +0000

    Added UserListActivity

--- app/src/main/java/com/example/carl/list/UserListActivity.java
+++ app/src/main/java/com/example/carl/list/UserListActivity.java
@@ -1,12 +1,15 @@
 package com.example.carl.list;
 
 import android.app.ListActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 
-public class HomeActivity extends ListActivity {
+/**
+ * Created by Carl on 10/11/2016.
+ */
+
+public class UserListActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
